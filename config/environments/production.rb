7b427e5c54b584f5cafe0d18d4bd1eca123c1427
rails <rails@rails.com>
2014-10-17 16:13:19 +0530

    emailmg.domain.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "emailmg.domain.com",
     :port                 => 587,
     :user_name            => 'info@hiperpool.com',
     :password             => 'Info2014',
